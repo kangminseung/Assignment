5bc092f44589e669dccba975e00314aa697f3022
kangminseung <31638864+kangminseung@users.noreply.github.com>
2017-09-06 16:27:05 +0900

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class assignment {
+	public static void main(String args[]) {
+		System.out.println("Student ID Number : 2014301001");
+		System.out.println("Name : Kang Min Seung");
+		System.out.println("Mobile Phone Number : 010-5595-6504");
+		System.out.println("GitHub Address : vnttkf333@naver.com");
+	}
+}
